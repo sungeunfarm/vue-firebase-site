cfb2397b2d6062c9c4c103f10c89ec0b624edac3
corinicode <corincode@gmail.com>
2022-06-07 16:35:23 +0900

    3

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,21 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <v-app-bar-nav-icon @click="drawer = !drawer" />
-        <SiteTitle :title="title"/>
+        <SiteTitle :title="site.title"/>
         <v-spacer/>
-      <v-btn icon to="/about">
+
         <v-btn @click="save"><v-icon>mdi-check</v-icon></v-btn>
-      </v-btn>
+        <v-btn @click="read" class="ml-3"><v-icon small >리드</v-icon></v-btn>
+        <v-icon small @click="readOne" class="ml-3">mdi-check</v-icon>
+
     </v-app-bar>
     <v-content>
       <v-navigation-drawer app v-model="drawer">
-        <SiteMenu />
+        <SiteMenu :items="site.menu"/>
       </v-navigation-drawer>
       <router-view />
     </v-content>
-    <FooterView :footer="footer"/>
+    <FooterView :footer="site.footer"/>
   </v-app>
 </template>
 
@@ -28,12 +30,50 @@ export default {
   data () {
     return {
       drawer: false,
-      title: '나만의 사이트 만들기',
-      footer: 'CodeEats Company.'
+      site: {
+        menu: [],
+        title: '나만의 사이트 만들기',
+        footer: 'CodeEats Company.'
+      }
     }
+  },
+  created () {
+    this.subscribe()
   },
   mounted () {
     console.log(this.$firebase)
+  },
+  methods: {
+    subscribe () {
+      // 리스너 형태는 트라이캐치로 에러처리가 안되고 마지막 에러메세지 확인을 만들어놓는다.
+      this.$firebase.database().ref().child('site').on('value', (sn) => {
+        const v = sn.val()
+        if (!v) {
+          this.$firebase.database().ref().child('site').set(this.site)
+        }
+        this.site = v
+      }, (e) => {
+        console.log(e.message)
+      })
+    },
+
+    save () {
+      console.log('save@@@')
+      this.$firebase.database().ref().child('abcd').set({
+        title: 'abcd', text: 'tttttt'
+      })
+    }, // 실시간 리스닝
+    read () {
+      this.$firebase.database().ref().child('abcd').on('value', (sn) => {
+        console.log(sn)
+        console.log(sn.val())
+      })
+    },
+    // readOne 한번만 읽어온다
+    async readOne () {
+      const sn = await this.$firebase.database().ref().child('abcd').once('value')
+      console.log(sn.val())
+    }
   }
 }
 </script>
