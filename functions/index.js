7d79a0f219e5153040cbfc47eec06d0b4c9281a2
corinicode <corincode@gmail.com>
2022-06-09 01:26:23 +0900

    7

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,22 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://vue-firebase-code-default-rtdb.firebaseio.com",
+});
+
+const db = admin.database()
+
+exports.createUser = functions.auth.user().onCreate(async (user) => {
+    const { uid,email,displayName,photoURL} = user
+    const u = {email,displayName,photoURL,createAt:newDAte}
+    db.ref().child(uid).set(u)
+});
+exports.deleteUser = functions.auth.user().onDelete(async (user) => {
+    const { uid} = user
+    db.ref().child(uid).remove()
+});
